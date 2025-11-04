d01238b6a41db5342c96b60e3df84ffc7e375e0f
gefakra <61078526+gefakra@users.noreply.github.com>
2025-11-04 09:47:06 +0300

    Add files via upload
    стартовый, тестовый вариант

---
+++ Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using MiniBlog.Data;
+using MiniBlog.Interface;
+using MiniBlog.Service;
+
+namespace MiniBlog
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                 options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddScoped<IPostRepository,PostRepository>();
+            builder.Services.AddScoped<PostService>();
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
